827b56628f03a138be7deb14ed15b02f08b6c1d7
Ubuntu <ubuntu@ip-172-31-90-24.ec2.internal>
2020-03-12 02:53:57 +0000

    lodown revisions

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -22,51 +22,46 @@ var _ = require("lodown-mlittle17");
  */
 
 var maleCount = function(array) {
-    // I: an array
-    // O: number
-    // C. use _.filter
     
-    
-    
-    // get access to each customer object and filter out the males
-//     let arrOfMaleObj = _.filter(array, function(customerObj){
-//         // checking to see if customer object is a male using the gender key
-//         return customerObj.gender === "male";
-//     })
-//     let number = arrOfMaleObj.length // chain on .length to get number of male customers
-//     console.log(number);
-//     return number;
-// };
+// get access to each customer object and filter out the males
 
-return _.filter(array, function(customerObj, index, array) {
-    return customerObj.gender === "male";
-    
-}).length
+// using filter function, taking in the customers array
+    // also taking in a function that uses the element that is a customerObj
+        // will compile an array of customerObj's that are male
+return _.filter(customers, function(customerObj) {
+    // returning the length of the array of customerObj's that are male
+    return customerObj.gender === "male";}).length
 };
 
 
 var femaleCount = function(array) {
-    let arrOfFmaleObj = _.filter(array, function(customerObj){
-        return customerObj.gender === "female";
-    }
-    )
-    let number = arrOfFmaleObj.length
-    return number;
-    
-    
+// get access to each customer object and filter out the females
+
+// using filter function, taking in the customers array
+    // also taking in a function that uses the element that is a customerObj
+        // will compile an array of customerObj's that are female
+return _.filter(customers, function(customerObj) {
+    // returning the length of the array of customerObj's that are female
+    return customerObj.gender === "female";}).length
+
 };
 
 var oldestCustomer = function(array) {
+    // creating two variables that will update as we loop through the customers
     let name
     let age = 0;
-
-    _.each(array,function(value,index, array) {
-        
-        if(value.age > age) {
-            age = value.age
-            name = value.name
+    // using each to loop through the array of customers
+        // function takes in the customerObj
+    _.each(customers,function(customerObj) {
+        // if the given customerObj's age is greater than current age...
+        if(customerObj.age > age) {
+            // age is now equal to that object's age
+            age = customerObj.age
+            // name is now equal to that object's name
+            name = customerObj.name
         }
     })
+    // return name once loop is complete
     return name
 }
 
@@ -76,51 +71,59 @@ var oldestCustomer = function(array) {
     
 
 var youngestCustomer = function(array) {
+    // creating two variables that will update as we loop through the customers
     let name
     let age = 100;
-    
-    _.each(array, function(value, index, array) {
-        
-        if(value.age < age) {
-            age = value.age
-            name = value.name
+    // using each to loop through the array of customers
+        // function takes in the customerObj
+    _.each(customers, function(customerObj) {
+        // if the given customerObj's age is less than current age...
+        if(customerObj.age < age) {
+            // age is now equal to that object's age
+            age = customerObj.age
+            // name is now equal to that object's name
+            name = customerObj.name
         }
         
     })
+    // return name once loop is complete
     return name;
-    
-    
-    
     
 };
 
 var averageBalance = function(array) {
 
-// Find the average balance of all customers
-
-let balances = _.pluck(array, "balance");
-// console.log(balances);
+// creating a variable named balances that will compile all customer balances
+let balances = _.pluck(customers, "balance");
+// creating an empty array that will contain balances as numbers
 let numBalances = [];
-
+// looping through balances array
 for (let i = 0; i < balances.length; i++) {
+  // creating balString that holds each balance temporarily
+    // first replaces all number symbols with an empty string
   let balString = balances[i].replace("$", "")
+    // re-assigning balString with a string that has replaced commas with an empty string
     balString = balString.replace(",", "")
+    // pushes that cleaned up string to numBalances as a number
     numBalances.push(Number(balString));
 }
-
+// creating total variable with value of 0
 let total = 0; 
-
+// for loop that adds each element to total
 for(let i = 0; i < numBalances.length; i++) {
     total += numBalances[i];
 }
 
+// returning final value of total, divided the length of elements within balances
 return total/numBalances.length;
 
 }
 
 var firstLetterCount = function(array,letter) {
+    // creating lowLetter to "equalize" the input letter
     var lowLetter = letter.toLowerCase();
-    return _.filter(array, function(customerObj, index, array) {
+    // using filter 
+    return _.filter(array, function(customerObj) {
     return customerObj.name[0].toLowerCase() === lowLetter;
     
 }).length
@@ -176,7 +179,40 @@ var topThreeTags = function(customers) {
         }, []
     )
   
-    console.log(tagsArr);
+    let tagsObj = tagsArr.reduce(function(tagObj, tag) {
+        if(tagObj[tag]) {
+            tagObj[tag] += 1;
+        } else {
+            tagObj[tag] = 1;
+        } 
+            //console.log(tagObj);
+            return tagObj;
+        
+    }, {});
+    
+    let subArr = (Object.entries(tagsObj));
+    let sorted = subArr.sort(function(a, b){
+        return a[1] > b[1]})
+    
+    let topThree = sorted.slice(-3);
+    let top3Tags = [];
+    for(let i = 0; i < topThree.length; i++) {
+        top3Tags.push(topThree[i][0])
+        
+    }
+    
+    return top3Tags;
+    
+    
+    // let arr = _.map(tagsObj, function(key) {
+    //     let kvArr = [];
+    //     kvArr.push(tagsObj);
+    //     return kvArr;
+    // }
+  
+    // )
+    
+    // console.log(arr);
     
 }
 
