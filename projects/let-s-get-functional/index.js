47dcc5b227c4b76acdf8d43397f3e6548e3e92fb
Ubuntu <ubuntu@ip-172-31-90-24.ec2.internal>
2020-03-12 04:51:45 +0000

    LGF psuedocode

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -122,44 +122,46 @@ return total/numBalances.length;
 var firstLetterCount = function(array,letter) {
     // creating lowLetter to "equalize" the input letter
     var lowLetter = letter.toLowerCase();
-    // using filter 
+    // using filter over the array 
+        // function takes in each customerObj
     return _.filter(array, function(customerObj) {
+    // returning all customerObj's in an array whose name starts with the input letter
+        
     return customerObj.name[0].toLowerCase() === lowLetter;
-    
+     // returned in an array, so returing the length of that array to find the number
 }).length
 };
 
  
-/**
- * ### 7: `friendFirstLetterCount`
- - **Objective**: Find how many friends of a given customer have names that start with a given letter
- - **Input**: `Array`, `Customer`, `Letter`
- - **Output**: `Number`
- - **Constraints**:
- */
- 
 
  var friendFirstLetterCount = function(customers, customerName, letter) {
-    // console.log(customerName);
     // using reduce to return the customerObj that matches the input string
+        // using customerMatch as seed, looping through customerObj
     let customer = _.reduce(customers, function(customerMatch, customerObj) {
+        // checks if customerName is equal to any customerObj's name
         if(customerName === customerObj.name) {
+            // returns that customerObj if true
             return customerObj
+        // assigns customerMatch to customer variable
         } return customerMatch;
-        
-        
     }) 
-    // using firstLetterCount function to check the array of customer
-        // returning a number of friends that match that letter
+    // using firstLetterCount function to check the array of customer friends
+        // returning a number of friends that match the input letter
     return firstLetterCount(customer.friends, letter);
  }
 
 var friendsCount = function(customers, customerName) {
-   
+   // using reduce to loop over the customers array
+    // creating a seed named friendsList, making it's initial value an empty array
+        // using customerObj as "current value"
     return _.reduce(customers, function(friendsList, customerObj) {
+         // using pluck tp put all the values of name of a given customerObj's friends list in an array
+            // also using .includes to check if that array from pluck includes the customerName
          if(_.pluck(customerObj.friends, "name").includes(customerName)) {
+             // will push that name into friendsList if the conditional resolves to true
              friendsList.push(customerObj.name);
          };
+            // returning friendsList once done
             return friendsList;
     }, [])
     
@@ -169,64 +171,68 @@ var friendsCount = function(customers, customerName) {
     
 
 var topThreeTags = function(customers) {
-    // console.log(customers, "in");
-    // returning the top three tags amongst all customers
+    
+    // using reduce to create an array of all tags within every customerObj
+        // allTags will be an array of sub-arrays, with each customerObj's tags
     let tagsArr = customers.reduce(function(allTags, customerObj) {
+            // assigning flat the value of the allTags array
+                // using .concat to create one array that adds each array of tags to it
             var flat = allTags.concat(customerObj["tags"]);
-            // console.log(flat);
+            // returing the final array with all tag values (including duplicates) as elements
             return flat;
-            //console.log(allTags);
         }, []
     )
-  
+    
+    // creating an object that will include each tag as a key with their value being the number of occurances
+        // using reduce with empty object as seed
     let tagsObj = tagsArr.reduce(function(tagObj, tag) {
+        // if a given tag exists as a property in the object, add 1 to the value
         if(tagObj[tag]) {
             tagObj[tag] += 1;
+        // if a given tag does not exist in the object, create it with the value of 1
         } else {
             tagObj[tag] = 1;
         } 
-            //console.log(tagObj);
+           // returning the final object
             return tagObj;
         
     }, {});
     
+    // creating an array with sub-arrays of a tag and it's corresponding total
+        // ex. [["tag", 4],["tag2", 5]
     let subArr = (Object.entries(tagsObj));
+    // using sort method to arrange all sub-arrays by the value at the index of 1
+        // the higher the number, the further in the back of the array
     let sorted = subArr.sort(function(a, b){
         return a[1] > b[1]})
-    
+    // using slice to create an array of the top three sub-arrays
     let topThree = sorted.slice(-3);
+    // creating top3Tags as empty array
     let top3Tags = [];
+    // looping over topThree array, pushing in the element at the 0 index of i, which will be the tag
     for(let i = 0; i < topThree.length; i++) {
         top3Tags.push(topThree[i][0])
         
     }
-    
+    // returning top3Tags, an array of strings
     return top3Tags;
-    
-    
-    // let arr = _.map(tagsObj, function(key) {
-    //     let kvArr = [];
-    //     kvArr.push(tagsObj);
-    //     return kvArr;
-    // }
-  
-    // )
-    
-    // console.log(arr);
     
 }
 
 var genderCount = function(customers) {
     
-    
+    // using reduce to loop over the customers array
+        // creating a seed of genderCountObj, which is an empty object literal
     return customers.reduce(function(genderCountObj, customerObj) {
+        // if genderCountObj does include a given gender property, add 1 to it's value 
         if(genderCountObj[customerObj.gender]) {
             genderCountObj[customerObj.gender] += 1;
         } else {
+        // if genderCountObj does not include a given gender property, create it with a value of 1
             genderCountObj[customerObj.gender] = 1;
         }
+            // return genderCountObj, which will include the count of each gender
             return genderCountObj;
-            console.log(genderCountObj);
     }, {})
     
     
